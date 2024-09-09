4d1af3ac416ee9c8607e7a68e102e7caee35f374
AhmetMuratAcar <neistersenat@gmail.com>
2024-09-08 17:34:19 -0700

    add functionality to account for master message's various existance cases.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord import app_commands
-from utils.msgJSON import save_message_id, load_message_id
+from utils.message_utils import save_message_id, existence_check, load_message_id
 
 # Loading .env
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -28,9 +28,23 @@ async def on_ready():
 )
 @app_commands.checks.has_permissions(administrator=True)
 async def setup_master_message(interaction: discord.Interaction, channel: discord.TextChannel):
+    # Check if a master message is already set up
+    masterCheck = await existence_check(interaction)
+    if masterCheck:
+        data = load_message_id()
+        existing_channel = interaction.guild.get_channel(data['channel_id'])
+        await interaction.response.send_message(
+            f"Master message already exists in {existing_channel.mention}", 
+            ephemeral=True
+        )
+        return
 
     # Construct the message content
-    message_content = "Welcome to the server! Here are the current games, roles, and emojis:\n"
+    message_content = ("**Hello** :point_up: :nerd:\n\n"
+                       "\> Below are the currently available game roles.\n"
+                       "\> Interact with the emojis to add/remove roles from yourself.\n"
+                       "\> If you want to add a game use the `/add game` function and pass in the game name and emoji you want."
+                       )
 
     # Send the message to the specified channel and save its data as JSON
     master_message = await channel.send(message_content)
@@ -38,9 +52,10 @@ async def setup_master_message(interaction: discord.Interaction, channel: discor
 
     await interaction.response.send_message(f"Master message setup in {channel.mention}", ephemeral=True)
 
+
 @setup_master_message.error
 async def setup_master_message_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
-    """ Send an ephemeral message if the user lacks admin permissions """
+    """Send an ephemeral message if the user lacks admin permissions"""
     if isinstance(error, app_commands.errors.MissingPermissions):
         await interaction.response.send_message(
             "You do not have the required permissions (Administrator) to run this command.",
---
+++ utils/message_utils.py
@@ -0,0 +1,70 @@
+import json
+import os
+import discord
+
+DATA_DIR = DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data')
+
+# Making sure data directory exists
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+JSON_PATH = os.path.join(DATA_DIR, "master_message.json")
+
+
+def save_message_id(message_id: int, channel_id: int):
+    data = {
+        "message_id": message_id,
+        "channel_id": channel_id
+    }
+
+    with open(JSON_PATH, "w") as f:
+        json.dump(data, f)
+
+
+def load_message_id():
+    if os.path.exists(JSON_PATH):
+        with open(JSON_PATH, "r") as f:
+            return json.load(f)
+    
+    return None
+
+
+def delete_message_id():
+    if os.path.exists(JSON_PATH):
+        os.remove(JSON_PATH)
+
+
+async def existence_check(interaction: discord.Interaction):
+    """Goes through the various possibilities of master message's existance."""
+    data = load_message_id()
+    # If the path does not exist terminate early
+    if not data:
+        return False
+
+    # If the data exist but the message no longer exists
+    try:
+        # Fetch the channel from the saved channel_id
+        channel = interaction.guild.get_channel(data['channel_id'])
+
+        # The channel no longer exists
+        if not channel:
+            await interaction.response.send_message(
+                f"Old master message's channel was deleted, setting up new one.", 
+                ephemeral=True
+            )
+            delete_message_id()
+            return False
+
+        # Try to fetch the master message using the saved message_id
+        await channel.fetch_message(data['message_id'])
+
+        # If you get here the message and channel exist.
+        return True
+    
+    except:
+        await interaction.response.send_message(
+            f"Old master message was deleted, setting up new one.", 
+            ephemeral=True
+        )
+        delete_message_id()
+        return False
